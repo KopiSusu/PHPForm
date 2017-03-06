efe81c3aae2e2c973ad3069d1f0adfd3ebccc658
Komang Luce <KopiSusu@users.noreply.github.com>
2017-03-06 11:54:12 -0500

    Cleanup, See if this works

--- resultat.php
+++ resultat.php
@@ -1,11 +1,30 @@
 <?php 
 
   if (isset($_POST['submit'])) {
-    $mailContent = "Prénom: " . $_POST['prenom'] . "<br>" . "Nom: " . $_POST['nom'] . "<br>" . "Email: " . $_POST['email'] . "<br>" . "Objet: " . $_POST['objet'] . "<br>" . "Message: " . $_POST['msg']; 
-    echo $mailContent; 
+    // Set to field    
+    $to = "amelie.haladjian@gmail.com";
+    
+    // Set from field, this was missing in your original example    
+    $from = $_POST['email'];
+    
+    //Data (i laid it out more xplicitly so its easier to read), didnt change anything here this looked okay
+    $mailContent = "Prénom: "     .$_POST['prenom']  ."<br>\n";
+    $mailContent .= "Nom: "       .$_POST['nom']     ."<br>\n";
+    $mailContent .= "Email: "     .$_POST['email']   ."<br>\n"; 
+    $mailContent .= "Objet: "     .$_POST['objet']   ."<br>\n"; 
+    $mailContent .= "Message: "   .$_POST['msg']     ."<br>\n"; 
+    
+    //Hard Code Subject     
     $subject = "Form to mail";
-
-    mail("amelie.haladjian@gmail.com", $subject, $mailContent) ; 
+    
+    //Headers (not sure if this is requried but docs have them and they do use the "from" field which is normally important)
+    $headers  = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+    $headers .= "From: <".$from. ">" ;
+    
+    //Sends mail request structure (to, subject, message, headers)     
+    mail ($to, $subject, $mailContent, $headers); 
+    echo "sent";
   }
 
 ?>
